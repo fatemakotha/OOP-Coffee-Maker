a2e54b25750b53ae72f75ca8721300df1cf476e6
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-30 23:35:51 +0600

    Completed

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         print(drink)
-        if coffee_maker.is_resource_sufficient(drink):
-            print(money_machine.make_payment(drink.cost))
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
