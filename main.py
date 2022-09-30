5386955f984e03de2a818cdaf83966df916e4545
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-30 23:14:40 +0600

    is_on = True

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from money_machine import MoneyMachine
 money_machine = MoneyMachine() #Object created and stored inside money_machine
 coffee_maker = CoffeeMaker() #Object created and stored inside coffee_maker
 menu = Menu()
-
+is_on = True
 
 coffee_maker.report() #Taps into coffee_maker and pulls out the report
 money_machine.report() #taps into money_machine and pulls out the report
