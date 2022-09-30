e98fd5cb6c3cd6ee9b307551fb7044fb6a5fb334
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-30 23:14:14 +0600

    Object menu created

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine() #Object created and stored inside money_machine
 coffee_maker = CoffeeMaker() #Object created and stored inside coffee_maker
+menu = Menu()
+
 
 coffee_maker.report() #Taps into coffee_maker and pulls out the report
 money_machine.report() #taps into money_machine and pulls out the report
+
