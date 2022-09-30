2ecec742672cb1e3388ab3745ee25b111a3af8a6
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-30 23:08:47 +0600

    Object created and stored inside coffee_maker

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-money_machine = MoneyMachine() #Object created and stored inside my_money_machine
+money_machine = MoneyMachine() #Object created and stored inside money_machine
+coffee_maker = CoffeeMaker() #Object created and stored inside coffee_maker
 
-money_machine.report() #Taps into money_machine and pulls out the report
+money_machine.report() #taps into money_machine and pulls out the report
