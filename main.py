d3035df4d5d7ad14ebc3f9c93641f564d507c862
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-30 23:08:01 +0600

    Tap into money_machine and pulls out the report

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine() #Object created and stored inside my_money_machine
+
+money_machine.report() #Taps into money_machine and pulls out the report
